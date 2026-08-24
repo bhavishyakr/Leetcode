798c8ee564f97d4465ce2ea424b7b11ee8758a54
Bhavishya Kumar <bhavishyakr647@gmail.com>
2026-08-25 03:13:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (43.35%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -6,11 +6,6 @@ public:
             if(nums[i] == target || target <= nums[i])
                 return i;
         }
-        // for(int i=0 ; i<nums.size() ; i++){
-        //     if(target <= nums[i]){
-        //         return i;
-        //     }
-        // }
         return i;
     }
 };
