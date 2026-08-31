2d6d01d6573e72e544b30f29b33f583c7ddaba33
Bhavishya Kumar <bhavishyakr647@gmail.com>
2026-09-01 01:46:52 +0530

    Time: 2030 ms (5.86%) | Memory: 262.6 MB (32.01%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int sz) {
+        int k;
+        if(sz == nums.size() || nums.size() == 1) 
+            return;
+        else if(sz > nums.size()){
+            k = sz % nums.size();
+        }else{
+            k = sz;
+        }
+        vector<int> arr2;
+        for(int i=0; i<nums.size()-k ; i++)
+            arr2.push_back(nums[i]);  
+        int rem = nums.size()-k;
+        for(int j=0; j<k ; j++)
+            nums[j] = nums[rem++];
+        int i=0;
+        for(int m=k ; m<nums.size(); m++) 
+            nums[m] = arr2[i++]; 
+        for(int el : arr2)
+            cout<<el<<" "; 
+        }
+};
