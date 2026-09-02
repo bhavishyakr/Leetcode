9a44796c49ea91b2a33a95b8af4e6a621771a399
Bhavishya Kumar <bhavishyakr647@gmail.com>
2026-09-03 03:08:04 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (52.77%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -7,10 +7,7 @@ public:
         int k=1;
         for(int i=1; i<nums.size(); i++){
             if(nums[i] != nums[i-1]){
-                // el = nums[i];
                 nums[k++] = nums[i];
-            }else{
-                
             }
         }
         return k;
