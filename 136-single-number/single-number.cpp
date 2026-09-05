dd3287afc9fd1b64509bf4789683480cd7b6e992
Bhavishya Kumar <bhavishyakr647@gmail.com>
2026-09-05 19:09:15 +0530

    Time: 7 ms (18.16%) | Memory: 20.6 MB (58.62%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -3,38 +3,38 @@ public:
     int singleNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end());
 
-            int st = 0, end = nums.size()-1;
-            while(st <= end){
+    int st = 0, end = nums.size()-1;
+    while(st <= end){
 
-                int mid = (st+end) / 2;
+        int mid = (st+end) / 2;
 
-                if(nums.size() == 1)
-                    return nums[0];
+        if(nums.size() == 1)
+            return nums[0];
 
-                if(mid == 0 && nums[mid] != nums[mid+1])
-                    return nums[mid];
+        if(mid == 0 && nums[mid] != nums[mid+1])
+            return nums[mid];
 
-                if(mid == nums.size()-1 && nums[mid] != nums[mid-1])
-                    return nums[mid];
+        if(mid == nums.size()-1 && nums[mid] != nums[mid-1])
+            return nums[mid];
 
-                if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1])
-                    return nums[mid];
-                    
-                if( mid % 2 == 0 ){
-                    if(nums[mid] == nums[mid-1]){ 
-                    end = mid - 1;  
-                    }else{
-                        st = mid + 1;
-                    }
-                }else{  
-                    if(nums[mid] == nums[mid-1])
-                        st = mid + 1;
-                    else
-                        end = mid - 1;
-                    }
-                
+        if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1])
+            return nums[mid];
+
+        if( mid % 2 == 0 ){
+            if(nums[mid] == nums[mid-1]){ 
+            end = mid - 1;  
+            }else{
+                st = mid + 1;
             }
-        
-        return -1;
+        }else{  
+            if(nums[mid] == nums[mid-1])
+                st = mid + 1;
+            else
+                end = mid - 1;
+            }
+                
     }
+        
+return -1;
+}
 };
