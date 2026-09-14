2098c3121bb471913cac120e4cd90b4094479d60
Bhavishya Kumar <bhavishyakr647@gmail.com>
2026-09-15 02:05:53 +0530

    Time: 59 ms (5.27%) | Memory: 24.2 MB (40.86%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int>temp;
+        sort(nums.begin() , nums.end());
+        int freq = 0;
+        int el = nums[0];
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == el){
+                freq++;
+            }else{
+                freq = 1;
+                el = nums[i];
+            }
+
+            if(freq > nums.size()/3)
+                temp.push_back(nums[i]);
+
+        }
+        for(int val:temp)
+            cout<<val<<" ";
+        vector<int>ans;
+        if(temp.size()>0){
+            ans.push_back(temp[0]);
+            for(int i=1; i<temp.size(); i++)
+                if(temp[i] != temp[i-1]){
+                    ans.push_back(temp[i]);
+                }
+        }
+        return ans;
+    }
+};
