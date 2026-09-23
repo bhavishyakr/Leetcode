1068e8d1ab67c42c1887f7c8f8445c6652096134
Bhavishya Kumar <bhavishyakr647@gmail.com>
2026-09-24 03:11:23 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (15.64%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.cpp
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.cpp
@@ -5,18 +5,16 @@ public:
         vector<int>suffsum(nums.size(),0);
         presum[0] = 0;
         suffsum[nums.size()-1] = 0;
-        for(int i=1; i<nums.size(); i++){
+        for(int i=1; i<nums.size(); i++)
             presum[i] = nums[i-1] + presum[i-1];
-        }
-        for(int i=nums.size()-2; i>=0; i--){
+        
+        for(int i=nums.size()-2; i>=0; i--)
             suffsum[i] = nums[i+1] + suffsum[i+1];
-            cout<<presum[i]<<suffsum[i]<<" ";
-        }
+        
         for(int i=0; i<nums.size(); i++)
-            if(presum[i] == suffsum[i]){
-            cout<<presum[i]<<suffsum[i]<<" ";
+            if(presum[i] == suffsum[i])
                 return i;
-            }
+            
         return -1;
     }
 };
